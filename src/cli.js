173e330b7f4ca0a8a094e578682bc24c6b5103bc
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-26 23:25:58 -0500

    cli - added todos

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,9 @@
 import "@babel/polyfill";
 import installPkgs from './installPkgs';
 
+// TODO: add help text
+// TODO: add ability to download .eslintrc file
+
 // determine which airbnb config based on user input
 const airbnbConfigPkg = process.argv[2] === 'react' ? 'eslint-config-airbnb' : 'eslint-config-airbnb-base';
 
