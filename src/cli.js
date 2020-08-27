43190df1eacb58de0decc3f2e791cb6e0b9abba3
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-26 23:05:53 -0500

    split out installPkgs fn

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,9 @@
 #! /usr/bin/env node
-import shell from 'shelljs';
-import { getPeerDeps } from './index'
+import "@babel/polyfill";
+import installPkgs from './installPkgs';
 
 // determine which airbnb config based on user input
 const airbnbConfigPkg = process.argv[2] === 'react' ? 'eslint-config-airbnb' : 'eslint-config-airbnb-base';
 
-const installPkgs = async (airbnbConfigPkg) => {
-  // fetch and log packages to be installed
-  const pkgs = await getPeerDeps(airbnbConfigPkg);
-
-  // debug to make sure shell commands are working
-  shell.exec(`echo ${pkgs.join(' ')}`);
-
-  // run the shell command
-  // shell.exec(`npm i -D ${pkgsToInstall.join(' ')}`);
-}
-
+// install packages via npm
 installPkgs(airbnbConfigPkg);
