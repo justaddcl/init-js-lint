f2e860ce450c5940109bd1b41591522e9e1185ce
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-08-27 00:29:09 -0500

    1.1.0
    - drops shelljs dependency
    - splits code into modules
    - adds linting

---
+++ src/cli.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import '@babel/polyfill';
+import installPkgs from './installPkgs';
+
+// TODO: add help text
+// TODO: add ability to download .eslintrc file
+
+// determine which airbnb config based on user input
+const airbnbConfigPkg =
+  process.argv[2] === 'react'
+    ? 'eslint-config-airbnb'
+    : 'eslint-config-airbnb-base';
+
+// install packages via npm
+installPkgs(airbnbConfigPkg);
