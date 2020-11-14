0f760095e4c55150fec449e26030fb4b7fe00bb0
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-11-14 00:45:19 -0600

    webpack config - adds babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,22 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
   output: {
-    filename: 'index.min.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
       { test: /.js$/, loader: 'shebang2-loader' },
     ],
   },
